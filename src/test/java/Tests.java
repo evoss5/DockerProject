2cf78a5b95ce742d2eab487427f94cd786909ede
evoss5 <116517344+evoss5@users.noreply.github.com>
2023-11-30 22:42:09 +0100

    Aktualizacja

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,23 +1,17 @@
 import Pages.BasicPage;
-import Pages.FirstPage;
 import Pages.HomePage;
 import Pages.LoginPage;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
 
 public class Tests {
 
 
     WebDriver driver;
     Service service;
-    FirstPage page;
     BasicPage basic;
     HomePage home;
     WebDriverWait wait;
